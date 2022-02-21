6582a9ca049441ca8b10b780185a72d8c6c03928
Riccardo Zanei <rzanei@forte.io>
2022-02-21 17:35:52 +0100

    Added all events  to lib.rs  (WIP)

--- native/ilpstreaming/src/lib.rs
+++ native/ilpstreaming/src/lib.rs
@@ -1,17 +1,16 @@
 // imports
+use interledger::packet::ErrorCode;
 use bytes::{Bytes, BytesMut};
 use interledger::errors::{AccountStoreError, AddressStoreError, ExchangeRateStoreError};
 use interledger::packet::Address;
-use interledger::rates::ExchangeRateStore;
-use interledger::router::RouterStore;
 use interledger::service::{Account, AccountStore, AddressStore, Username};
 use interledger::service_util::MaxPacketAmountAccount;
-use interledger::stream::{PaymentNotification,ConnectionGenerator,StreamReceiverService}; //waypoint
 use rustler::types::binary::{Binary, OwnedBinary};
 use rustler::{Encoder, Env, Error, NifResult, Term};
 use std::boxed::Box;
 use std::collections::HashMap;
 use std::convert::TryFrom;
+use std::marker::PhantomData;
 use std::str::FromStr;
 
 // macro space
@@ -35,47 +34,288 @@ macro_rules! error {
     };
 }
 
-
 // functions..
+
 #[rustler::nif(schedule = "DirtyCpu")]
-fn encode_payment_notification<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
-    let m = arg.decode::<HashMap<String, Term>>().or(err!("could not decode the stream data to map<String,Term>"))?;
+fn encode_connection_close_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode the stream_connection_close_frame data to map<String,Term>"
+    ))?;
 
-            // get fields
-            let tu = m.get("to_username").ok_or(error!("to_username value is missing"))?;
-            let fu = m.get("from_username").ok_or(error!("from_username is missing"))?;
-            let d = m.get("destination").ok_or(error!("destination is missing"))?;
-            let a = m.get("amount").ok_or(error!("amount is missing"))?;
-            let t = m.get("timestamp").ok_or(error!("timestamp is missing"))?;
-            let s = m.get("sequence").ok_or(error!("sequence is missing"))?;
-            let cc = m.get("connection_closed").ok_or(error!("connection_closed is missing"))?;
+    // get fields
+    let c = m.get("code").ok_or(error!("code is missing"))?;
+    let m = m.get("from_username").ok_or(error!("from_username is missing"))?;
 
-            // transform
-            let to_usernames = tu.decode::<&str>().or(err!("could not decode to_username"))?;
-            let from_usernames = fu.decode::<&str>().or(err!("could not decode from_username"))?;
-            let destinations = d.decode::<&str>().or(err!("could not decode the destination"))?;
-            let amount = a.decode::<u64>().or(err!("could not decode the amount"))?;
-            let timestamp = t.decode::<String>().or(err!("could not decode the timestamp"))?;
-            let sequence = s.decode::<u64>().or(err!("could not decode the sequence"))?;
-            let connection_closed = cc.decode::<bool>().or(err!("could not decode connection_closed value "))?;
-
-            //special decode
-            let destination = Address::from_str(destinations).or(err!("could not decode the destination"))?;
-            let to_username = Username::from_str(to_usernames).or(err!("could not decode to_username"))?;
-            let from_username = Username::from_str(from_usernames).or(err!("could not decode from_username"))?;
-           
-
-            let pn = PaymentNotification {
-                to_username,
-                from_username,
-                destination,
-                amount,
-                timestamp,
-                sequence,
-                connection_closed,
-            };
-            Ok("No idea to return ".encode(env))  //to fix, WIP
-        }
+    // transform
+    let code = c.decode::<ErrorCode>().or(err!("could not decode to_username"))?; //Type ErrorCode
+    let message = m.decode::<&str>().or(err!("could not decode from_username"))?;
 
 
+    let pn = ConnectionCloseFrame { code, message };
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_connection_new_address<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode the stream_connection_new_address data to map<String,Term>"
+    ))?;
+
+    // get fields
+    let sa = m.get("source_account").ok_or(error!("source_account is missing"))?;
+
+    // transform
+    let source_accounts = sa.decode::<&str>().or(err!("could not decode source_account"))?;
+
+    //special decode
+    let source_account = Address::from_str(source_accounts).or(err!("could not decode the source_account"))?;
+
+    let pn = ConnectionNewAddressFrame { source_account };
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_connection_asset_details_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode the stream_cconnection_asset_details_frame data to map<String,Term>"
+    ))?;
+
+    // get fields
+    let sac = m.get("source_asset_code").ok_or(error!("source_asset_code is missing"))?;
+    let sas = m.get("source_asset_scale").ok_or(error!("source_asset_scale is missing"))?;
+
+    // transform
+    let source_asset_code = sac.decode::<&str>().or(err!("could not decode source_asset_code"))?;
+    let source_asset_scale = sas.decode::<u8>().or(err!("could not decode source_asset_code"))?;
+
+    let pn = ConnectionAssetDetailsFrame { source_asset_code, source_asset_scale };
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_connection_max_data_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode the stream_connection_max_data_frame to map<String,Term>"
+    ))?;
+
+    // get fields
+    let mo = m.get("max_offset").ok_or(error!("max_offset is missing"))?;
+  
+    // transform
+    let max_offset = mo.decode::<u64>().or(err!("could not decode max_offset"))?;
+    
+    let pn = ConnectionMaxDataFrame { max_offset };
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_connection_data_blocked_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode the stream_connection_data_blocked_frame to map<String,Term>"
+    ))?;
+
+    // get fields
+    let mob = m.get("max_offset").ok_or(error!("max_offset is missing"))?;
+  
+    // transform
+    let max_offset = mob.decode::<u64>().or(err!("could not decode max_offset"))?;
+    
+    let pn = ConnectionDataBlockedFrame { max_offset };
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_connection_connection_max_stream_id_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode the stream_connection_connection_max_stream_id_frame to map<String,Term>"
+    ))?;
+
+    // get fields
+    let msi = m.get("max_stream_id").ok_or(error!("max_stream_id is missing"))?;
+  
+    // transform
+    let max_stream_id = msi.decode::<u64>().or(err!("could not decode max_stream_id"))?;
+    
+    let pn = ConnectionMaxStreamIdFrame { max_stream_id };
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_connections_stream_id_blocked_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode the stream_connections_stream_id_blocked_frame to map<String,Term>"
+    ))?;
+
+    // get fields
+    let msis = m.get("max_stream_id").ok_or(error!("max_stream_id is missing"))?;
+  
+    // transform
+    let max_stream_id = msis.decode::<u64>().or(err!("could not decode max_stream_id"))?;
+    
+    let pn = ConnectionStreamIdBlockedFrame { max_stream_id };
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_stream_close_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode the stream_close_frame to map<String,Term>"
+    ))?;
+
+    // get fields
+    let si = m.get("stream_id").ok_or(error!("stream_id is missing"))?;
+    let c = m.get("code").ok_or(error!("code is missing"))?;
+    let m = m.get("stream_id").ok_or(error!("stream_id is missing"))?;
+  
+    // transform
+    let stream_id = si.decode::<u64>().or(err!("could not decode stream_id"))?;
+    let code = si.decode::<u64>().or(err!("could not decode code"))?; // Type ErrorCode 
+    let message = si.decode::<&str>().or(err!("could not decode message"))?;
+    
+    let pn = StreamCloseFrame { stream_id, code, message};
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
  
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_stream_money_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode the stream_money_frame to map<String,Term>"
+    ))?;
+
+    // get fields
+    let si = m.get("stream_id").ok_or(error!("stream_id is missing"))?;
+    let s = m.get("shares").ok_or(error!("shares is missing"))?;
+  
+    // transform
+    let stream_id = si.decode::<u64>().or(err!("could not decode stream_id"))?;
+    let shares = s.decode::<u64>().or(err!("could not decode shares"))?; // 
+    
+    let pn = StreamMoneyFrame { stream_id, shares};
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_stream_max_money_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode the stream_max_money_frame to map<String,Term>"
+    ))?;
+
+    // get fields
+    let si = m.get("stream_id").ok_or(error!("stream_id is missing"))?;
+    let rm = m.get("receive_max").ok_or(error!("receive_max is missing"))?;
+    let tr = m.get("total_received").ok_or(error!("total_received is missing"))?;
+
+  
+    // transform
+    let stream_id = si.decode::<u64>().or(err!("could not decode stream_id"))?;
+    let receive_max = rm.decode::<u64>().or(err!("could not decode receive_max"))?; 
+    let total_received = tr.decode::<u64>().or(err!("could not decode total_received"))?;
+
+    
+    let pn = StreamMaxMoneyFrame { stream_id, receive_max, total_received};
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_stream_money_blocked_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode the tream_money_blocked_frame to map<String,Term>"
+    ))?;
+
+    // get fields
+    let si = m.get("stream_id").ok_or(error!("stream_id is missing"))?;
+    let sm = m.get("send_max").ok_or(error!("send_max is missing"))?;
+    let ts = m.get("total_sent").ok_or(error!("total_received is total_sen"))?;
+
+  
+    // transform
+    let stream_id = si.decode::<u64>().or(err!("could not decode stream_id"))?;
+    let send_max = sm.decode::<u64>().or(err!("could not decode send_max"))?; 
+    let total_sent = ts.decode::<u64>().or(err!("could not decode total_sent"))?;
+
+    
+    let pn = StreamMoneyBlockedFrame { stream_id, send_max, total_sent};
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_stream_data_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode the stream_data_frame to map<String,Term>"
+    ))?;
+
+    // get fields
+    let si = m.get("stream_id").ok_or(error!("stream_id is missing"))?;
+    let o = m.get("offset").ok_or(error!("offset is missing"))?;
+    let d = m.get("data").ok_or(error!("data is total_sen"))?;
+
+  
+    // transform
+    let stream_id = si.decode::<u64>().or(err!("could not decode stream_id"))?;
+    let offset = o.decode::<u64>().or(err!("could not decode offset"))?; 
+    let data = d.decode::<u64>().or(err!("could not decode data"))?;
+
+    
+    let pn = StreamDataFrame { stream_id, offset, data};
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_stream_max_data_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode stream_max_data_frame to map<String,Term>"
+    ))?;
+
+    // get fields
+    let si = m.get("stream_id").ok_or(error!("stream_id is missing"))?;
+    let mo = m.get("max_offset").ok_or(error!("max_offset is missing"))?;
+ 
+
+  
+    // transform
+    let stream_id = si.decode::<u64>().or(err!("could not decode stream_id"))?;
+    let max_offset = mo.decode::<u64>().or(err!("could not decode max_offset"))?; 
+
+    
+    let pn = StreamMaxDataFrame { stream_id, max_offset};
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn encode_stream_data_blocked_frame<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
+    let m = arg.decode::<HashMap<String, Term>>().or(err!(
+        "could not decode stream_data_blocked_frame to map<String,Term>"
+    ))?;
+
+    // get fields
+    let sis = m.get("stream_id").ok_or(error!("stream_id is missing"))?;
+    let mos = m.get("max_offset").ok_or(error!("max_offset is missing"))?;
+ 
+
+  
+    // transform
+    let stream_id = sis.decode::<u64>().or(err!("could not decode stream_id"))?;
+    let max_offset = mos.decode::<u64>().or(err!("could not decode max_offset"))?; 
+
+    
+    let pn = StreamDataBlockedFrame { stream_id, max_offset};
+
+    Ok("No idea to return ".encode(env)) //to fix, WIP
+}
+
+//There is no Stream Receipt 0x17 { not implemented in Interleger-rs }
+
+
+
--- test/ilp_streaming_test.exs
+++ test/ilp_streaming_test.exs
@@ -1,8 +1,52 @@
-defmodule IlpStreamingTest do
+defmodule IlpRoutingTest do
   use ExUnit.Case
-  doctest IlpStreaming
+  doctest IlpRouting
 
-  test "greets the world" do
-    assert IlpStreaming.hello() == :world
+  test "encode/1 control request" do
+
+
+    assert {:error, "could not decode last_known_routing_table_id"} = IlpRouting.encode(%{
+      "type" => "control_request",
+      "features" => :some,
+      "last_known_epoch" => :some,
+      "last_known_routing_table_id" => :some,
+      "mode" => :some
+    })
+
+    assert {:error, "could not convert last_known_routing_table_id to list of bytes of size ROUTING_TABLE_ID_LEN"} = IlpRouting.encode(%{
+      "type" => "control_request",
+      "features" => ["hello", "there"],
+      "last_known_epoch" => 345345,
+      "last_known_routing_table_id" => [0],
+      "mode" => 0
+    })
+
+    assert {:error, "last_known_epoch not u32"} = IlpRouting.encode(%{
+      "type" => "control_request",
+      "features" => ["Test", "one"],
+      "last_known_epoch" => 79.33,
+      "last_known_routing_table_id" => [0],
+      "mode" => 0
+    })
+
+    assert is_list(IlpRouting.encode(IlpRoutingHelper.control_request))
+
   end
+
+  # test "encode/1 update request" do
+
+  #   assert is_list(IlpRouting.encode(IlpRoutingHelper.update_request))
+
+  # end
+
+  # test "encode decode control_request" do
+  #   control_request = IlpRoutingHelper.control_request
+  #   assert control_request == control_request |> IlpRouting.encode |> :binary.list_to_bin() |> IlpRouting.decode
+  # end
+
+  # test "encode decode update_request" do
+  #   update_request = IlpRoutingHelper.update_request
+  #   assert update_request == update_request |> IlpRouting.encode |> :binary.list_to_bin() |> IlpRouting.decode
+  # end
+
 end
